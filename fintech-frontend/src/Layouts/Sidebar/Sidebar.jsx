def67776f570f4f68d12ccb5f7df0f63635d87df
Sabah <sabahalmel7@gmail.com>
2023-11-30 19:00:23 +0200

    responsive sidebarand fixing colors

--- fintech-frontend/src/Layouts/Sidebar/Sidebar.jsx
+++ fintech-frontend/src/Layouts/Sidebar/Sidebar.jsx
@@ -4,25 +4,51 @@ import DashboardIcon from "@rsuite/icons/legacy/Dashboard";
 import GroupIcon from "@rsuite/icons/legacy/Group";
 import "./Sidebar.css";
 function Sidebar() {
+  const [expanded, setExpanded] = React.useState(true);
+  const [activeKey, setActiveKey] = React.useState("1");
   return (
-    <div className="sidebar">
-      <Sidenav appearance="">
+    <div>
+      <Sidenav appearance="" className="sidebar" expanded={expanded}>
         <Sidenav.Body>
-          <Nav activeKey="1">
-            <Nav.Item eventKey="1" className="item" icon={<DashboardIcon />}>
+          <Nav activeKey={activeKey} onSelect={setActiveKey}>
+            <Nav.Item
+              eventKey="1"
+              className="item"
+              icon={
+                <DashboardIcon style={{ fontSize: "25px", height: "25px" }} />
+              }
+            >
               Profile
             </Nav.Item>
-            <Nav.Item eventKey="2" className="item" icon={<DashboardIcon />}>
+            <Nav.Item
+              eventKey="2"
+              className="item"
+              icon={
+                <DashboardIcon style={{ fontSize: "25px", height: "25px" }} />
+              }
+            >
               Dashboard
             </Nav.Item>
-            <Nav.Item eventKey="3" className="item" icon={<GroupIcon />}>
+            <Nav.Item
+              eventKey="3"
+              className="item"
+              icon={<GroupIcon style={{ fontSize: "25px", height: "25px" }} />}
+            >
               Campaign
             </Nav.Item>
-            <Nav.Item eventKey="4" className="item" icon={<GroupIcon />}>
+            <Nav.Item
+              eventKey="4"
+              className="item"
+              icon={<GroupIcon style={{ fontSize: "25px", height: "25px" }} />}
+            >
               Donation
             </Nav.Item>
           </Nav>
         </Sidenav.Body>
+        <Sidenav.Toggle
+          expanded={expanded}
+          onToggle={(expanded) => setExpanded(expanded)}
+        />
       </Sidenav>
     </div>
   );
