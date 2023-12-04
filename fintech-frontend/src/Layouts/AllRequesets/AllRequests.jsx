694a5f1d43a1819a60923d10d3c4735c0b81e92a
Sabah <sabahalmel7@gmail.com>
2023-12-04 09:43:02 +0200

    delete

---
+++ fintech-frontend/src/Layouts/AllRequesets/AllRequests.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Slide from "../../Components/Dashboard/Slide";
+
+function AllRequests() {
+  return (
+    <div>
+      <h1 style={{ margin: "30px" }}>All Requests</h1>
+      <Slide />
+    </div>
+  );
+}
+
+export default AllRequests;
